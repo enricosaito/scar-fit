22b8a636a8a5628f1d6d3d9e66fe5347005d5bd0
Enrico <107450323+enricosaito@users.noreply.github.com>
2025-04-03 14:01:29 -0300

    feat: update app layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,35 @@
 // app/_layout.tsx
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { ThemeProvider } from "./context/ThemeContext";
 import "../global.css";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <ThemeProvider>
+      <StatusBar style="light" />
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="profile"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="notifications"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="tracking"
+          options={{
+            headerShown: false,
+            presentation: "modal",
+          }}
+        />
+      </Stack>
+    </ThemeProvider>
+  );
 }
