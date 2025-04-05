498b7ed06d8adeb5f986bc07915418d0732c9088
Enrico <107450323+enricosaito@users.noreply.github.com>
2025-04-05 15:18:19 -0300

    feat: add authbypass util

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,18 @@
 // app/lib/utils.ts
 import { twMerge } from "tailwind-merge";
 import { clsx, type ClassValue } from "clsx";
+import { Platform } from "react-native";
+import Constants from "expo-constants";
+
+export function isRunningInSimulator(): boolean {
+  // Check if we're on iOS and in development
+  if (Platform.OS === "ios" && __DEV__) {
+    // iOS simulators usually have specific model identifiers
+    // or we can check if we're in development mode
+    return Constants.executionEnvironment !== "standalone";
+  }
+  return false;
+}
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
