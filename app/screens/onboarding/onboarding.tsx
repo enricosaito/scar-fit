aed22ecfc5bb3f01ffcb8122fb9670076ee4348f
Enrico <107450323+enricosaito@users.noreply.github.com>
2025-04-18 20:00:46 -0300

    fix: update onboarding redirect screen route

--- app/screens/onboarding/onboarding.tsx
+++ app/screens/onboarding/onboarding.tsx
@@ -3,5 +3,5 @@ import React from "react";
 import { Redirect } from "expo-router";
 
 export default function OnboardingRedirect() {
-  return <Redirect href="/screens/onboarding/index" />;
+  return <Redirect href="/screens/onboarding" />;
 }
