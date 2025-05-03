3ad7f795906a07b7c71baaa40998087f8b4def85
Enrico Saito <enricosaito@gmail.com>
2025-05-03 02:36:16 -0300

    fix: update supabase api key

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,17 +1,25 @@
 // app/lib/supabase.ts
-import 'react-native-url-polyfill/auto'
 import { createClient } from "@supabase/supabase-js";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import customStorageAdapter from "./secureStorage";
 
 const supabaseUrl = "https://ssrklevifozwowhpumvu.supabase.co";
-const supabaseAnonKey = "your-anon-key";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzcmtsZXZpZm96d293aHB1bXZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM4MDc4NzcsImV4cCI6MjA1OTM4Mzg3N30.0rwKDJUkJMDggk27C0Cx08ldg_4FNWgBnQjssb9uhzc";
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.warn("Missing Supabase credentials.");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: AsyncStorage,
     autoRefreshToken: true,
     persistSession: true,
-    detectSessionInUrl: false,
+    detectSessionInUrl: true,
+    storage: customStorageAdapter,
+  },
+  global: {
+    headers: {
+      "x-app-version": "1.0.0",
+    },
   },
 });
 
